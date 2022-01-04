9013f4fbf4d298b5b568688ea5639946e99f4fd3
hnaji-el <hamidnaji981@gmail.com>
2022-01-04 16:51:39 +0100

    ...

--- Day02/ex00/Fixed.cpp
+++ Day02/ex00/Fixed.cpp
@@ -1,6 +1,17 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnaji-el <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/04 12:18:58 by hnaji-el          #+#    #+#             */
+/*   Updated: 2022/01/04 16:49:14 by hnaji-el         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+ 
 #include "Fixed.hpp"
-
+ 
 Fixed::Fixed(void) : _fixedPtVal(0)
 {
 	std::cout << "Default constructor called" << std::endl;
@@ -12,7 +23,7 @@ Fixed::~Fixed(void)
 	std::cout << "Destructor called" << std::endl;
 	return ;
 }
-
+ 
 Fixed::Fixed(Fixed const & src)
 {
 	std::cout << "Copy constructor called" << std::endl;
@@ -23,8 +34,23 @@ Fixed::Fixed(Fixed const & src)
 Fixed&	Fixed::operator=(Fixed const & rhs)
 {
 	std::cout << "Assignation operator called" << std::endl;
-	this->_fixedPtVal = rhs.getRawBits();
-	return (*this);
+	if (this != &rhs)
+		this->_fixedPtVal = rhs.getRawBits();
+	return *this;
 }
 
-int	const	Fixed::_nbFraBits = 8;
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedPtVal);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fixedPtVal = raw;
+	return ;
+}
+
+int const	Fixed::_nbFrBits = 8;
+
--- Day02/ex00/Fixed.hpp
+++ Day02/ex00/Fixed.hpp
@@ -1,24 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnaji-el <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/04 12:20:04 by hnaji-el          #+#    #+#             */
+/*   Updated: 2022/01/04 16:45:06 by hnaji-el         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef FIXED_H
 # define FIXED_H
 
 # include <iostream>
-
+ 
 class Fixed
 {
 private:
-	int				 _fixedPtVal;
-	static int const  _nbFraBits;
-
+	int	_fixedPtVal;
+	static int const _nbFrBits;
+ 
 public:
 	Fixed(void);
 	~Fixed(void);
 	Fixed(Fixed const & src);
-
+ 
 	Fixed&	operator=(Fixed const & rhs);
 
-	int 	getRawBits(void) const;
+	int		getRawBits(void) const;
 	void	setRawBits(int const raw);
 };
-
+ 
 #endif
